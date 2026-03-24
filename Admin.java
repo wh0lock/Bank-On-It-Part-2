f5e6c2f239356646cce2f3b2180dfc69a3bd5263
Emily Adams <emilyalexisadams@gmail.com>
2026-03-24 14:45:30 -0400

    Update

--- Admin.java
+++ Admin.java
@@ -1,36 +1,43 @@
 import java.util.*;
 import java.io.*;
 
-public class Admin extends User{
+public class Admin extends User implements HasMenu, Serializable{
+
+  public static void main(String[] args) {
+    Admin a = new Admin();
+    a.login();
+  } // end main
+
   public Admin(){
-    String userName = "admin";
-    String PIN = "0000";
-    this.userName = userName;
-    this.PIN = PIN;
+    this.userName = "admin";
+    this.PIN = "0000";
   } // end constructor
 
   public String menu(){
     Scanner input = new Scanner(System.in);
+    System.out.println("  Admin Menu  ");
+    System.out.println();
     System.out.println("0) Exit");
     System.out.println("1) Full customer report");
     System.out.println("2) Add user");
     System.out.println("3) Apply interest to savings accounts");
     System.out.println();
     System.out.print("Action: ");
+
     String response = input.nextLine();
     return response;
   } // end menu
 
   public void start(){
-    
+    // implemented in Bank
   } // end start
 
   public String getReport(){
-    Scanner input = new Scanner(System.in);
-    System.out.println("Admin report");
-    String report = input.nextLine();
-    System.out.println("User: " + userName + "PIN: " + PIN);
+    String report = "Admin user name: " + this.getUserName();
+    report += ", Admin PIN: " + this.getPIN();
+
     return report;
   } // end getReport
+
 } // end Admin
 
--- Bank.java
+++ Bank.java
@@ -1,17 +1,47 @@
 import java.util.*;
 import java.io.*;
 
-public class Bank implements HasMenu(){
+public class Bank implements HasMenu {
   Admin admin = new Admin();
-  ArrayList<String> Customers = new ArrayList<String>();
+  CustomerList customers = new CustomerList();
+
+  public static void main(String[] args){
+    new Bank();
+  } // end main
 
   public Bank(){
-    //this.loadSampleCustomers();
-    //this.saveCustomers();
-    this.loadCustomers();
     this.start();
-    this.saveCustomers();
   } // end constructor
 
-  
+  public String menu(){
+    Scanner input = new Scanner(System.in);
+    System.out.println("Bank Menu");
+    System.out.println();
+    System.out.println("0) Exit");
+    System.out.println("1) Login as admin");
+    System.out.println("2) Login as customer");
+    System.out.println();
+    System.out.print("Action: ");
+    String response = input.nextLine();
+    return response;
+  } // end menu
 
+  public void start(){
+    boolean keepGoing = true;
+    while (keepGoing){
+      String response = menu();
+      if (response.equals("0")){
+        keepGoing = false;
+      } else if (response.equals("1")){
+          System.out.println("Admin login");
+        } else if (response.equals("2")){
+            System.out.println("Customer login");
+          } else {
+              System.out.println("Please enter 0, 1, or 2");
+            } // end if
+    } // end while
+  } // end start
+
+} // end Bank
+
+class CustomerList extends ArrayList<Customer> {};
